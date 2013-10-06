eb0b794207f3a1c715f0ea2362f0e7441ccb4e0a
TM Lee <tm89lee@gmail.com>
2013-10-06 23:45:32 +0800

    Supports widen up gist.github.com code

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
 var widen = false;
 
 chrome.browserAction.onClicked.addListener(function(tab){
-	var regexPage = new RegExp(/https:\/\/github.com\//);
-  var match = regexPage.exec(tab.url);
-  if(match && tab.status === 'complete' && !widen) {
+	var githubRegex = new RegExp(/https:\/\/github.com\//);
+  var gistRegex = new RegExp(/https:\/\/gist.github.com\//);
+  var githubMatch = githubRegex.exec(tab.url);
+  var gistMatch = gistRegex.exec(tab.url);
+  if((githubMatch || gistMatch) && tab.status === 'complete' && !widen) {
 		chrome.tabs.insertCSS(tab.id, {
       file: "widen.css"
   	});
